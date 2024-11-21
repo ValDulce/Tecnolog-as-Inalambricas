d31353cf66a5468a1564135de39cfe7f42a41612
Alejandro Leon <leon05032003@gmail.com>
2024-11-20 20:27:49 -0600

    codigos de la practica de la unidad 3

---
+++ P_unidad3/P_unidad3/P_unidad3/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace P_unidad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCon = new System.Windows.Forms.Button();
+            this.txtAlgo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnCon
+            // 
+            this.BtnCon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCon.Location = new System.Drawing.Point(158, 81);
+            this.BtnCon.Name = "BtnCon";
+            this.BtnCon.Size = new System.Drawing.Size(129, 31);
+            this.BtnCon.TabIndex = 0;
+            this.BtnCon.Text = "CONECTAR";
+            this.BtnCon.UseVisualStyleBackColor = true;
+            this.BtnCon.Click += new System.EventHandler(this.BtnCon_Click);
+            // 
+            // txtAlgo
+            // 
+            this.txtAlgo.Location = new System.Drawing.Point(94, 137);
+            this.txtAlgo.Name = "txtAlgo";
+            this.txtAlgo.Size = new System.Drawing.Size(239, 20);
+            this.txtAlgo.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "PRACTICA UNIDAD 3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(459, 240);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAlgo);
+            this.Controls.Add(this.BtnCon);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCon;
+        private System.Windows.Forms.TextBox txtAlgo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ P_unidad3/P_unidad3/P_unidad3/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P_unidad3
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Port;
+        bool isClosed = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Port = new System.IO.Ports.SerialPort();
+            Port.PortName = "COM3"; /*Checar el com */
+            Port.BaudRate = 9600;
+            Port.ReadTimeout = 500;
+            try
+            {
+                Port.Open();
+            }
+            catch { }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        Thread Hilo = new Thread(EsucharSerial);
+                Hilo.Start();
+            }
+        private void BtnCon_Click(object sender, EventArgs e)
+        {
+               BtnCon.Enabled = false;
+                BtnCon.SendToBack();
+
+            }
+
+            private void EsucharSerial()
+
+            {
+                while (!isClosed)
+                {
+                    try
+                    {
+                        string cadena = Port.ReadLine();
+                        txtAlgo.Invoke(new MethodInvoker(
+                            delegate
+                            {
+                                txtAlgo.Text = cadena;
+
+                            }
+
+                            ));
+
+                    }
+                    catch { }
+                }
+            }
+
+            private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+            {
+                isClosed = true;
+                Close();
+        }
+
+        
+    }
+
+    }
